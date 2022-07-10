9393a55c3ffe2a5d27d3612328a2f31770bdf62a
stodoriko <s.todorikos@gmail.com>
2022-07-10 20:14:15 +0300

    added homework

---
+++ task.js
@@ -0,0 +1,7 @@
+function compareArrays(arr1, arr2) {
+  return arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]); 
+}
+
+function advancedFilter(arr1) {
+  return arr1.filter(item => item > 0 && item % 3 === 0).map(item => item * 10);
+}
